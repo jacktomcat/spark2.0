6a9149836ff4c066e5c39b7bef79c301063bdc9d
zhuhuihui <jackjboss@gmail.com>
2017-07-11 18:02:42 +0800

    parse csv by 2.0

--- src/main/java/com/gochinatv/spark/SqlWindow.scala
+++ src/main/java/com/gochinatv/spark/SqlWindow.scala
@@ -1,6 +1,8 @@
 package com.gochinatv.spark
 
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
+
 
 /**
   * Created by jacktomcat on 2017/7/11.
@@ -8,8 +10,23 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SqlWindow {
 
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setAppName("SparkSqlWindow").setMaster("local[*]")
-    val sc = new SparkContext(sparkConf)
+    // val sparkConf = new SparkConf().setAppName("SparkSqlWindow").setMaster("local[*]")
+    // val sc = new SparkContext(sparkConf)
+
+    val sparkSession = SparkSession.builder().master("local[*]").appName("SparkSqlWindow").getOrCreate()
+    val sqlContext = sparkSession.sqlContext
+    val dataFrame = sqlContext.read
+      .option("encoding","UTF-8")
+      .option("header",true)
+      .option("timestampFormat","yyyy-MM-dd HH:mm:ss")
+      .csv("d:/logs.csv")
+
+    import org.apache.spark.sql.functions._
+
+    dataFrame//.select(dataFrame("id"),dataFrame("name"),dataFrame("time"))
+      .filter("id>1")
+      .groupBy(dataFrame("id"),dataFrame("name"),dataFrame("time"))
+      .agg(sum(dataFrame("value"))/sum(dataFrame("count"))).show(1000)
 
   }
 
